e8084fc4dddbbef8b4f3b12057a718a13d7c94a8
foxxy <ves.nikita1303@gmail.com>
2022-02-22 12:32:07 +0300

    remove old hot loader method

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.less';
+
+const App = () =>{
+  return (
+    <div>
+      <h1 className="h1-text">hello World</h1>
+      <h2 className="h1-text">React + TypeScript!</h2>
+    </div>
+  );
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-if ((module as any).hot) {
-    (module as any).hot.accept();
-}
-
 ReactDOM.render(<App/>, document.getElementById('root'));
