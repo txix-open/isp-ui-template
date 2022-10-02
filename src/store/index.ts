389f09c86ab4bba89dc29080df82dc56d8dae588
veselovnk <ves.nikita1303@gmail.com>
2022-10-02 18:17:13 +0300

    MSP-1053 create globalStore

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import { postService} from '../services/PostService';
+import userReducer from './reducers/UserSlice';
+
+const rootReducer = combineReducers({
+  userReducer,
+  [postService.reducerPath]: postService.reducer,
+});
+
+export const setupStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(postService.middleware),
+  });
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
